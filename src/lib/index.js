fe65a9318b4ccf8b121d3fa2c9e0e25a072e2728
fabiomangano <fabio_mangano@hotmail.it>
2020-01-10 10:29:55 +0100

    Update lib file

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,25 +1,24 @@
 const axios = require('axios');
 const xml2js = require('xml2js');
-const parser = new xml2js.Parser();
 const fx = require('money');
 const accounting = require('accounting');
-const { exchangeRatesArrayToObject } = require('../utils');
+const { normalizeExchangeRatesArray } = require('../utils');
+const parser = new xml2js.Parser();
 
-fx.base = 'EUR';
-
-const syncEuroExchangeRates = async () => {
+const syncEuroExchangeRates = async() => {
   const res = await axios.get('https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml');
   const parsedXml = await parser.parseStringPromise(res.data);
   const exchangeRates = parsedXml['gesmes:Envelope'].Cube[0].Cube;
-  return exchangeRatesArrayToObject(exchangeRates);
+  return normalizeExchangeRatesArray(exchangeRates);
 };
 
-const convertAmount =  (exchangeRatesForSelectedDay, amount, src_currency, dest_currency) => {
+const convertAmount = (exchangeRatesForSelectedDay, amount, src_currency, dest_currency) => {
+  fx.base = 'EUR';
   fx.rates = exchangeRatesForSelectedDay;
   let converted_amount = fx(amount).from(src_currency).to(dest_currency);
   converted_amount = accounting.toFixed(converted_amount, 2);
   return converted_amount;
-}
+};
 
 module.exports = {
   syncEuroExchangeRates,
