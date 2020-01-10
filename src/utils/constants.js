0b0922952660f45f35c46e2d9f8395d55c12ddaa
fabiomangano <fabio_mangano@hotmail.it>
2020-01-10 10:29:12 +0100

    Move some constants to specific file

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+const acceptedCurrencies = /(EUR|USD|JPY|BGN|CZK|DKK|GBP|HUF|PLN|RON|SEK|CHF|ISK|NOK|HRK|RUB|TRY|AUD|BRL|CAD|CNY|HDK|IDR|ILS|INR|KRW|MXN|MYR|NZD|PHP|SGD|THB|ZAR)$/;
+const currencies = [ 'EUR', 'USD', 'JPY', 'BGN', 'CZK', 'DKK', 'GBP', 'HUF', 'PLN', 'RON', 'SEK', 'CHF', 'ISK', 'NOK', 'HRK', 'RUB', 'TRY', 'AUD', 'BRL', 'CAD', 'CNY', 'HDK', 'IDR', 'ILS', 'INR', 'KRW', 'MXN', 'MYR', 'NZD', 'PHP', 'SGD', 'THB', 'ZAR', 'HKD'];
+
+module.exports = {
+  acceptedCurrencies,
+  currencies,
+};
