d583f59cce6b60ea548c08e826e3450e7ee86b62
fabiomangano <fabio_mangano@hotmail.it>
2020-01-10 10:27:16 +0100

    Create function to normalize exchangeRatesArray structure

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,19 +1,19 @@
 module.exports = {
   exchangeRatesArrayToObject: exchangeRatesArray => exchangeRatesArray.reduce((exchangeRates, item) => {
-    let dailyExchangeRatesArray = item.Cube;
-    dailyExchangeRatesArray.push({'$': { currency: 'EUR', rate: '1' }})
-    let dailyExchangeRatesObj = dailyExchangeRatesArray.reduce((res, cur) => {
-      let currency = cur['$'].currency;
-      let rate = parseFloat(cur['$'].rate, 10);
-      res[currency] = rate;
-      return res;
-    }, {});
-    exchangeRates[item['$'].time] = dailyExchangeRatesObj;
+    exchangeRates[item.currency] = item.rate;
     return exchangeRates;
-  }, {})
+  }, {}),
+
+  normalizeExchangeRatesArray: exchangeRatesArray => exchangeRatesArray.reduce((exchangeRates, item) => {
+    let dailyExchangeRatesArray = item.Cube;
+    dailyExchangeRatesArray.push({ $: { currency: 'EUR', rate: '1' } });
+    const normalized = dailyExchangeRatesArray.map(exchangeRate => {
+      return {
+        currency: exchangeRate['$'].currency,
+        rate: parseFloat(exchangeRate['$'].rate, 10),
+        referenceDate: item['$'].time,
+      };
+    });
+    return exchangeRates.concat(normalized);
+  }, []),
 };
-
-
-
-
-
