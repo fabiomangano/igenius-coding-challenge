9bf48eb1a3255f84377263cc3f1ed910f657db38
fabiomangano <fabio_mangano@hotmail.it>
2020-01-10 10:33:11 +0100

    Update controllers file with call to db

--- src/apis/controllers.js
+++ src/apis/controllers.js
@@ -1,9 +1,10 @@
 const { check, validationResult } = require('express-validator');
+const { acceptedCurrencies } = require('../utils/constants');
+const { currencyRate } = require('../models/currencyRate');
+const { exchangeRatesArrayToObject } = require('../utils');
 const { convertAmount } = require('../lib');
 
-const regEx = /(EUR|USD|JPY|BGN|CZK|DKK|GBP|HUF|PLN|RON|SEK|CHF|ISK|NOK|HRK|RUB|TRY|AUD|BRL|CAD|CNY|HDK|IDR|ILS|INR|KRW|MXN|MYR|NZD|PHP|SGD|THB|ZAR)$/;
-
-const convert = euroExchangeRates => (req, res) => {
+const convert = o => async(req, res) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -13,11 +14,18 @@ const convert = euroExchangeRates => (req, res) => {
   try {
     const { amount, src_currency, dest_currency, reference_date } = req.query;
 
-    if (!euroExchangeRates[reference_date]) {
-      return res.status(404).json({ errors: { message: 'Exchange rates non available for specified reference_date' } });
+    const euroExchangeRates = await currencyRate.findAll({
+      where: { referenceDate: reference_date },
+      raw: true,
+    });
+
+    if (euroExchangeRates.length === 0) {
+      return res.status(404).json({ errors: { message: 'Exchange rates non available for requested reference_date' } });
     };
 
-    let converted_amount = convertAmount(euroExchangeRates[reference_date], amount, src_currency, dest_currency);
+    const exchangeRatesObj = exchangeRatesArrayToObject(euroExchangeRates);
+
+    const converted_amount = convertAmount(exchangeRatesObj, amount, src_currency, dest_currency);
 
     res.json({
       src_currency,
@@ -43,20 +51,20 @@ const validate = method => {
           .isCurrency().withMessage('invalid amount format'),
         check('src_currency')
           .exists().withMessage('missing src_currency')
-          .matches(regEx).withMessage('invalid src_currency'),
+          .matches(acceptedCurrencies).withMessage('invalid src_currency'),
         check('dest_currency')
           .exists().withMessage('missing dest_currency')
-          .matches(regEx).withMessage('invalid dest_currency'),
+          .matches(acceptedCurrencies).withMessage('invalid dest_currency'),
         check('reference_date')
           .exists().withMessage('missing reference_date')
           .isISO8601().withMessage('invalid reference_date'),
-      ]
+      ];
     }
   }
-}
+};
 
 module.exports = {
   convert,
   validate,
-}
+};
 
