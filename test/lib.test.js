8b0e4b180a8943c4bc78c6bfd2f25623cf45cc60
fabiom93 <63911405+fabiom93@users.noreply.github.com>
2021-05-23 18:33:24 +0200

    Added todo

--- test/lib.test.js
+++ test/lib.test.js
@@ -1,16 +1,14 @@
-const chai = require('chai');
-const { convertAmount } = require('../src/lib');
-const { rates } = require('./fixtures/currencyRates');
+const chai = require("chai");
+const { convertAmount } = require("../src/lib");
+const { rates } = require("./fixtures/currencyRates");
 
 chai.should();
 
-describe('[Lib]', () => {
-  it('should correctly convert a requested amount from EUR to USD', (done) => {
-    const convertedAmount = convertAmount(rates, 10, 'EUR', 'USD');
-    convertedAmount.should.be.a('String');
-    convertedAmount.should.be.eql('7.45');
+describe("[Lib]", () => {
+  it("should correctly convert a requested amount from EUR to USD", (done) => {
+    const convertedAmount = convertAmount(rates, 10, "EUR", "USD");
+    convertedAmount.should.be.a("String");
+    convertedAmount.should.be.eql("7.45");
     done();
   });
 });
-
-	
